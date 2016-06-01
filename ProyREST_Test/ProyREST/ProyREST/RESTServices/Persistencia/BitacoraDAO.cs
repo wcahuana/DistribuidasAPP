ac67843acbb8f032a6520737bff5cc5f847aa8f4
wcahuana <w.cahuana.d@gmail.com>
2016-05-31 22:06:36 -0500

    Karen

---
+++ ProyREST_Test/ProyREST/ProyREST/RESTServices/Persistencia/BitacoraDAO.cs
@@ -0,0 +1,98 @@
+﻿using RESTServices.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using WCFCliente.Dominio;
+
+namespace RESTServices.Persistencia
+{
+    public class BitacoraDAO
+    {
+        public Bitacoras RegistrarBitacora(Bitacoras proveedorACrear)
+        {
+            
+            Bitacoras proveedorErroneo = null;
+            bool estado = false;
+            String nombreProcedimiento = "[XIUX_CEDICSEM_INSERTA_TIPIFICACION]";
+
+            SqlParameter[] parameter = {
+              new SqlParameter("@CEDICSEM_T_CONNID",proveedorACrear.DNI),
+              new SqlParameter("@CEDICSEM_T_ANI",proveedorACrear.Origen),
+              new SqlParameter("@CEDICSEM_T_DNIS",proveedorACrear.Destino),
+              new SqlParameter("@CEDICSEM_T_HORA_IN_LLAMADA",proveedorACrear.FechaInicioCall),
+              new SqlParameter("@CEDICSEM_T_HORA_OUT_LLAMADA",proveedorACrear.FechaTerminoCall),
+              new SqlParameter("@CEDICSEM_T_DURACION_LLAMADA",proveedorACrear.FechaInicioCall),
+              new SqlParameter("@CEDICSEM_T_AGENTE",proveedorACrear.Agente),
+              new SqlParameter("@CEDICSEM_T_TIPO_INTERACCION",proveedorACrear.TipoInteraccion),
+              new SqlParameter("@CEDICSEM_T_NOMBRE_CAMP",proveedorACrear.NombreCamp),
+              new SqlParameter("@CEDICSEM_T_cmbEfectividad",proveedorACrear.Efectividad),
+              new SqlParameter("@CEDICSEM_T_cmbAccion",proveedorACrear.Accion),
+              new SqlParameter("@CEDICSEM_T_cmbMotivos",proveedorACrear.Motivo),
+              new SqlParameter("@CEDICSEM_T_OBSERVACIONES",proveedorACrear.Observaciones),
+              new SqlParameter("@CEDICSEM_T_ESTADOINTERACCION",proveedorACrear.TipoInteraccion),
+              new SqlParameter("@CEDICSEM_T_ESTADOTIPIFICACION",proveedorACrear.TipoInteraccion),  
+              new SqlParameter("@CEDICSEM_T_FECHAACCION",proveedorACrear.FechaAccion),
+              new SqlParameter("@CEDICSEM_T_PREVCONNID",proveedorACrear.DNI)
+            };
+            try
+            {
+                estado = SqlHelper.executeProcedure(nombreProcedimiento, parameter);
+                return proveedorACrear;
+            }
+            catch (Exception error)
+            {
+
+                // Extract some information from this exception, and then 
+                // throw it to the parent method.
+                if (error.Source != null)
+                    Console.WriteLine("IOException source: {0}", error.Source);
+                return proveedorErroneo;
+                throw;
+
+            }
+            
+            
+        }
+
+
+        public List<Bitacoras> ConsultarBitacora(string DNI){
+            String sql = "";
+            DataTable datos;
+           
+
+            
+            List<Bitacoras> bitacorasEncontrados = new List<Bitacoras>();
+            Bitacoras bitacoraEncontrada = null;
+            sql += "exec [SXXX_EDICSEM_HISTORIAL] @Documento";
+                                
+            using (SqlConnection con = new SqlConnection(SqlHelper.Cadena))
+            {
+                con.Open();
+                using (SqlCommand com = new SqlCommand(sql, con))
+                {
+                    com.Parameters.Add(new SqlParameter("@Documento", DNI));
+                    using (SqlDataReader resultado = com.ExecuteReader())
+                    {
+                        while (resultado.Read())
+                        {
+                            bitacoraEncontrada = new Bitacoras()
+                            {
+                                DNI = (string)resultado["CEDICSEM_C_TxtDocumento"],
+                                FechaInicioCall = (string)resultado["FECHA"],
+                                FechaTerminoCall = (string)resultado["HORA"],
+                                Agente = (string)resultado["AGENTE"],
+                                Accion = (string)resultado["Combos"],
+                                Observaciones = (string)resultado["Observaciones"]
+                            };
+                            bitacorasEncontrados.Add(bitacoraEncontrada);
+                        }
+                    }
+                }
+            }
+            return bitacorasEncontrados;
+        }
+    }
+}
---
+++ ProyREST_Test/ProyREST/ProyREST/RESTTests/BitacorasTest.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+using System.IO;
+using System.Web.Script.Serialization;
+
+namespace RESTTests
+{
+    /// <summary>
+    /// Descripción resumida de BitacorasTest
+    /// </summary>
+    [TestClass]
+    public class BitacorasTest
+    {
+
+        [TestMethod]
+        public void InsertarBitacoraOk()
+        {
+            
+           
+              //pRUEBA DE CREACION DE CLIENTE via HTTP POST   
+            string postData = "{\"DNI\":\"11112222\",\"Destino\":\"12443\",\"FechaInicioCall\":\"121\",\"FechaTerminoCall\":\"xxxxxx\",\"DuracionCall\":\"xxxxxx\",\"Agente\":\"xxxxxx\",\"TipoInteraccion\":\"xxxxxx\",\"NombreCamp\":\"xxxxxx\",\"Accion\":\"xxxxxx\",\"FechaAccion\":\"xxxxxx\",\"Origen\":\"xxxxxx\",\"Observaciones\":\"xxxxxx\",\"Motivo\":\"xxxxxx\",\"Efectividad\":\"xxxxxx\",\"ID\":\"xxxxxx\"}"; //JSON
+
+              byte[] data = Encoding.UTF8.GetBytes(postData);
+              HttpWebRequest req = (HttpWebRequest)WebRequest.Create("http://localhost:9805/Bitacora.svc/Bitacoras");
+
+              req.Method = "POST";
+              req.ContentLength = data.Length ; 
+              req.ContentType = "application/json";
+
+              var reqStream = req.GetRequestStream();
+              reqStream.Write(data, 0, data.Length);
+
+              var res = (HttpWebResponse)req.GetResponse();
+              StreamReader reader = new StreamReader(res.GetResponseStream());
+              string bitacoraJson = reader.ReadToEnd();
+              JavaScriptSerializer js= new JavaScriptSerializer();
+
+              Bitacoras bitacoraCreada = js.Deserialize<Bitacoras>(bitacoraJson);
+              Assert.AreEqual("11112222", bitacoraCreada.DNI);
+             
+
+
+        }
+
+        [TestMethod]
+        public void InsertarBitacoraKO()
+        { 
+            try
+            {
+                string postData = "{\"DNI\":\"123\",\"Destino\":\"12443\",\"FechaInicioCall\":\"121\",\"FechaTerminoCall\":\"xxxxxx\",\"DuracionCall\":\"xxxxxx\",\"Agente\":\"xxxxxx\",\"TipoInteraccion\":\"xxxxxx\",\"NombreCamp\":\"xxxxxx\",\"Accion\":\"xxxxxx\",\"FechaAccion\":\"xxxxxx\",\"Origen\":\"xxxxxx\",\"Observaciones\":\"xxxxxx\",\"Motivo\":\"xxxxxx\",\"Efectividad\":\"xxxxxx\",\"ID\":\"xxxxxx\"}"; //JSON
+
+                byte[] data = Encoding.UTF8.GetBytes(postData);
+                HttpWebRequest req = (HttpWebRequest)WebRequest.Create("http://localhost:9805/Bitacora.svc/Bitacoras");
+
+                req.Method = "POST";
+                req.ContentLength = data.Length;
+                req.ContentType = "application/json";
+
+                var reqStream = req.GetRequestStream();
+                reqStream.Write(data, 0, data.Length);
+
+                var res = (HttpWebResponse)req.GetResponse();
+                StreamReader reader = new StreamReader(res.GetResponseStream());
+                string bitacoraJson = reader.ReadToEnd();
+                JavaScriptSerializer js = new JavaScriptSerializer();
+
+                Bitacoras bitacoraCreada = js.Deserialize<Bitacoras>(bitacoraJson);
+                Assert.AreEqual("123", bitacoraCreada.DNI);
+            }
+            catch (WebException e)
+            {
+                HttpStatusCode code = ((HttpWebResponse)e.Response).StatusCode;
+                string message = ((HttpWebResponse)e.Response).StatusDescription;
+                StreamReader reader = new StreamReader(e.Response.GetResponseStream());
+                string error = reader.ReadToEnd();
+                JavaScriptSerializer js = new JavaScriptSerializer();
+                string mensaje = js.Deserialize<string>(error);
+                Assert.AreEqual("El DNI tiene que ser de 8 Digitos", mensaje);
+            }
+
+
+
+
+        }
+
+        
+        [TestMethod]
+
+        public void ListarBitacora()
+        {
+            
+              HttpWebRequest req2 = (HttpWebRequest)WebRequest
+                .Create("http://localhost:9805/Bitacora.svc/Bitacoras/07954988");
+            req2.Method = "GET";
+            HttpWebResponse res2 = (HttpWebResponse)req2.GetResponse();
+            StreamReader reader2 = new StreamReader(res2.GetResponseStream());
+            string clienteJson3 = reader2.ReadToEnd();
+            JavaScriptSerializer js3 = new JavaScriptSerializer();
+            List<Bitacoras> clienteObtenido = js3.Deserialize<List<Bitacoras>>(clienteJson3);
+            Assert.AreEqual("07954988", clienteObtenido[0].DNI);
+        }
+
+        [TestMethod]
+        public void ListarNoExiste()
+        {
+            try
+            {
+                HttpWebRequest req2 = (HttpWebRequest)WebRequest
+                  .Create("http://localhost:9805/Bitacora.svc/Bitacoras/1234");
+                req2.Method = "GET";
+                HttpWebResponse res2 = (HttpWebResponse)req2.GetResponse();
+                StreamReader reader2 = new StreamReader(res2.GetResponseStream());
+                string clienteJson3 = reader2.ReadToEnd();
+                JavaScriptSerializer js3 = new JavaScriptSerializer();
+                List<Bitacoras> clienteObtenido = js3.Deserialize<List<Bitacoras>>(clienteJson3);
+                Assert.AreEqual("1234", clienteObtenido[0].DNI);
+            }
+            catch (WebException e)
+            {
+                HttpStatusCode code = ((HttpWebResponse)e.Response).StatusCode;
+                string message = ((HttpWebResponse)e.Response).StatusDescription;
+                StreamReader reader = new StreamReader(e.Response.GetResponseStream());
+                string error = reader.ReadToEnd();
+                JavaScriptSerializer js = new JavaScriptSerializer();
+                string mensaje = js.Deserialize<string>(error);
+                Assert.AreEqual("No se encontraron Datos", mensaje);
+            }
+        }
+        
+
+
+
+
+    }
+}
